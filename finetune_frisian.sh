3524a0461ad3aa3a7fc83dee91752e188ca55834
William Galvin <williamgalvin246@gmail.com>
2024-02-25 21:40:48 -0800

    relative data path

--- finetune_frisian.sh
+++ finetune_frisian.sh
@@ -4,7 +4,7 @@
     --model_name_or_path=gpt2 \
     --save_total_limit=2 \
     --do_train \
-    --train_data_file="/home/william/Documents/classes/493/final/data/fry_news_2020_100K/clean_fry_news_2020_100K-sentences.txt" \
+    --train_data_file="data/fry_news_2020_100K/clean_fry_news_2020_100K-sentences.txt" \
     --per_gpu_train_batch_size=1 \
     --overwrite_output_dir \
     --overwrite_cache \
